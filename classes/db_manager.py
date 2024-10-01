a0e5c706c406e57390be72e3266d23fa37259b6f
user <dolmatova3010@yandex.ru>
2024-10-01 13:52:43 +0700

    Moduls

---
+++ classes/db_manager.py
@@ -0,0 +1,85 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+from typing import List
+
+load_dotenv()
+
+
+class DBManager:
+    """Класс для подключния к БД, в котором реализованы различные выборки данных"""
+    def __init__(self, db_name: str) -> None:
+        """Инициализатор класса"""
+        self.__db_name = db_name
+
+    def __execute_query(self, query: str) -> List:
+        """Метод, в котором выполняется подключение к БД и выполнение передаваемого в метод запроса"""
+        conn = psycopg2.connect(dbname=self.__db_name, user=os.getenv("user"), password=os.getenv("password"),
+                                host=os.getenv("host"), port=os.getenv("port"))
+
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute(query)
+                result = cur.fetchall()
+        conn.close()
+        return result
+
+    def get_companies_and_vacancies_count(self) -> List:
+        """Метод, который получает список всех компаний и количество вакансий у каждой компании."""
+        query = '''
+        SELECT employer.name, employer.all_vacancies
+        FROM employer
+
+        GROUP BY employer.name, employer.all_vacancies
+        ORDER BY employer.all_vacancies DESC;
+        '''
+        return self.__execute_query(query)
+
+    def get_all_vacancies(self) -> List:
+        """Метод, который  получает список всех вакансий с указанием названия компании,
+        названия вакансии и зарплаты и ссылки на вакансию."""
+        query = '''
+        SELECT employer.name AS company, vacancy.name AS vacancy, 
+               vacancy.salary_from, vacancy.salary_to, vacancy.url
+        FROM vacancy
+        JOIN employer ON vacancy.employer_id = employer.id;
+        '''
+        return self.__execute_query(query)
+
+    def get_avg_salary(self) -> int:
+        """Метод, который получает среднюю зарплату по вакансиям."""
+        query = '''
+        SELECT AVG((salary_from + salary_to) / 2) AS average_salary
+        FROM vacancy
+        WHERE salary_from > 0 OR salary_to > 0;
+        '''
+        result = self.__execute_query(query)
+        average_salary = int(round(result[0][0]))
+        return average_salary
+
+    def get_vacancies_with_higher_salary(self) -> List:
+        """Метод, который получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
+        query = '''
+        SELECT employer.name AS company, vacancy.name AS vacancy, 
+               vacancy.salary_from, vacancy.salary_to, vacancy.url
+        FROM vacancy
+        JOIN employer ON vacancy.employer_id = employer.id
+        WHERE (salary_from + salary_to) / 2 > (
+            SELECT AVG((salary_from + salary_to) / 2)
+            FROM vacancy
+            WHERE salary_from > 0 OR salary_to > 0
+        );
+        '''
+        return self.__execute_query(query)
+
+    def get_vacancies_with_keyword(self, keyword: str) -> List:
+        """Метод, который получает список всех вакансий, в названии которых содержатся переданные в метод слова"""
+        query = f'''
+        SELECT employer.name AS company, vacancy.name AS vacancy, 
+               vacancy.salary_from, vacancy.salary_to, vacancy.url
+        FROM vacancy
+        JOIN employer ON vacancy.employer_id = employer.id
+        WHERE vacancy.name LIKE '%{keyword}%';
+        '''
+        return self.__execute_query(query)
+    
---
+++ src/utils.py
@@ -0,0 +1,84 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+from classes.api_hh import HHParser
+
+load_dotenv()
+
+conn_params = {
+    "host": os.getenv('host'),
+    "database": 'postgres',
+    "user": os.getenv('user'),
+    "password": os.getenv('password'),
+    "port": os.getenv('port')
+}
+# conn_params = {
+#   "host": "localhost",
+#   "database": "postgres",
+#   "user": "postgres",
+#   "password": "12345"
+# }
+
+def create_db(db_name: str) -> None:
+    """Функция для создания БД"""
+    conn = psycopg2.connect(**conn_params)
+
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    cur.execute(f"DROP DATABASE IF EXISTS {db_name}")
+    cur.execute(f"CREATE DATABASE {db_name}")
+
+    cur.close()
+    conn.close()
+
+create_db("test")
+
+
+def create_table(db_name: str) -> None:
+    """Функция для создания таблиц employer и vacancy"""
+    conn = psycopg2.connect(dbname=db_name, user=os.getenv("user"), password=os.getenv("password"),
+                            host=os.getenv("host"), port=os.getenv("port"))
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute('''CREATE TABLE employer (
+            id INT PRIMARY KEY,
+            all_vacancies INT,
+            name VARCHAR(255))''')
+
+            cur.execute('''CREATE TABLE vacancy (
+            id INT PRIMARY KEY,
+            name VARCHAR(255),
+            salary_from INT,
+            salary_to INT,
+            url VARCHAR(255),
+            employer_id INT REFERENCES employer(id))''')
+
+    conn.close()
+
+
+def insert_data(db_name: str) -> None:
+    """Функция для заполнения таблиц данными, полученными от АПИ"""
+    conn = psycopg2.connect(dbname=db_name, user=os.getenv("user"), password=os.getenv("password"),
+                            host=os.getenv("host"), port=os.getenv("port"))
+    with conn:
+        with conn.cursor() as cur:
+            hh = HHParser()
+            employers = hh.get_employers()
+            for employer in employers:
+                employer_id = employer['id']
+                cur.execute('INSERT INTO employer VALUES (%s, %s, %s)', (employer['id'],
+                                                                         employer['open_vacancies'], employer['name']))
+                vacancies = hh.get_vacancies(employer_id)
+                for vacancy in vacancies:
+                    if not vacancy['salary']:
+                        salary_from = 0
+                        salary_to = 0
+                    else:
+                        salary_from = vacancy['salary']['from'] if vacancy['salary']['from'] else 0
+                        salary_to = vacancy['salary']['to'] if vacancy['salary']['to'] else 0
+                    cur.execute('INSERT INTO vacancy VALUES (%s, %s, %s, %s, %s, %s)',
+                                (vacancy['id'], vacancy['name'], salary_from, salary_to,
+                                 vacancy['alternate_url'], employer_id))
+
+    conn.close()
